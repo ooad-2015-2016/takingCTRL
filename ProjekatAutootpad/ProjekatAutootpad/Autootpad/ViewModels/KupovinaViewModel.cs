b2f3b9f6cdb3c8fe0e2234023684cb8055eb5d7a
edoimamovic <eimamovic1@etf.unsa.ba>
2016-04-28 00:49:24 +0200

    započet pregled artikala ulogovanog korisnika

--- ProjekatAutootpad/ProjekatAutootpad/Autootpad/ViewModels/KupovinaViewModel.cs
+++ ProjekatAutootpad/ProjekatAutootpad/Autootpad/ViewModels/KupovinaViewModel.cs
@@ -10,6 +10,16 @@ namespace ProjekatAutootpad.Autootpad.ViewModels
     class KupovinaViewModel
     {
         public Korisnik User { get; set; }
+        //public 
+        public List<Dio> SviDijelovi { get
+            {
+                using (var db = new OtpadDbContext())
+                {
+                    return db.Dijelovi.ToList();
+                }
+            }
+        }
+        public List<Dio> Narudzba { get; set; }
         
         public KupovinaViewModel(PocetnaViewModel pvm)
         {
