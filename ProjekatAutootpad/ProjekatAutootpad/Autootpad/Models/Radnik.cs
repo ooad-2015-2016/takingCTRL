2c1ae1a010af3d21312127e3f619d06c7ee1aec5
Alma Hodzic <ahodzic3@etf.unsa.ba>
2016-04-28 03:35:22 +0200

    Dodana klasa Radnik (naslijeđena iz Korisnik) bila potrebna za ViewModel

---
+++ ProjekatAutootpad/ProjekatAutootpad/Autootpad/Models/Radnik.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjekatAutootpad.Autootpad.Models
+{
+    class Radnik: Korisnik
+    {
+        public List<Servis> rasporedServisa { get; set; };
+
+    }
+}
