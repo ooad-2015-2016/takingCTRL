31ed0a69e3a56d64a9ebc33ce039ad5f03b5ea0f
Alma Hodzic <ahodzic3@etf.unsa.ba>
2016-04-30 19:42:27 +0200

    Dodana Migracija

--- ProjekatAutootpad/ProjekatAutootpad/Migrations/20160430174056_Migracija.Designer.cs
+++ ProjekatAutootpad/ProjekatAutootpad/Migrations/20160430174056_Migracija.Designer.cs
@@ -7,9 +7,19 @@ using ProjekatAutootpad.Autootpad.Models;
 namespace ProjekatAutootpadMigrations
 {
     [ContextType(typeof(OtpadDbContext))]
-    partial class OtpadDbContextModelSnapshot : ModelSnapshot
+    partial class Migracija
     {
-        public override void BuildModel(ModelBuilder builder)
+        public override string Id
+        {
+            get { return "20160430174056_Migracija"; }
+        }
+
+        public override string ProductVersion
+        {
+            get { return "7.0.0-beta6-13815"; }
+        }
+
+        public override void BuildTargetModel(ModelBuilder builder)
         {
             builder
                 .Annotation("ProductVersion", "7.0.0-beta6-13815");
