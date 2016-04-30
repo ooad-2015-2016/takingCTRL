7d3a9dd50cfa2c9aad43f196866b779e9412c5a3
Alma Hodzic <ahodzic3@etf.unsa.ba>
2016-04-30 22:19:52 +0200

    dodana metoda mozeLiSeDodati

--- ProjekatAutootpad/ProjekatAutootpad/Autootpad/ViewModels/ZahtjevZaServisRegistrovaniKupacViewModel.cs
+++ ProjekatAutootpad/ProjekatAutootpad/Autootpad/ViewModels/ZahtjevZaServisRegistrovaniKupacViewModel.cs
@@ -1,4 +1,5 @@
-﻿using ProjekatAutootpad.Autootpad.Models;
+﻿using ProjekatAutootpad.Autootpad.Helper;
+using ProjekatAutootpad.Autootpad.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,13 +16,18 @@ namespace ProjekatAutootpad.Autootpad.ViewModels
 
         public void spasiZahtjevZaServis()
         {
-            //Dodaj = new RelayCommand<object>(dodaj, true);
+            Dodaj = new RelayCommand<object>(dodaj, mozeLiSeDodati);
         }
 
         public void dodaj(object parametar)
         {
             var db = new OtpadDbContext();
             db.narudzbeServisa.Add(narudzbaServisa);
+        }
+
+        public bool mozeLiSeDodati(object parametar)
+        {
+            return true;
         }
     }
 }
