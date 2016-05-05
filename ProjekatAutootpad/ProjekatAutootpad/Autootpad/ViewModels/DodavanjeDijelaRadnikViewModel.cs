d178fcab088a993b979a2f05a4f2f84722c61092
Alma Hodzic <ahodzic3@etf.unsa.ba>
2016-05-05 22:45:30 +0200

    Dodan view model Dodavanje autodijela od strane radnika iz liste ponuđenih autodijelova (koje su ponudili korisnici sistema)

--- ProjekatAutootpad/ProjekatAutootpad/Autootpad/ViewModels/DodavanjeDijelaRadnikViewModel.cs
+++ ProjekatAutootpad/ProjekatAutootpad/Autootpad/ViewModels/DodavanjeDijelaRadnikViewModel.cs
@@ -8,20 +8,46 @@ namespace ProjekatAutootpad.Autootpad.ViewModels
 {
     class DodavanjeDijelaRadnikViewModel
     {
-        public List<Dio> ponudeniAutodijelovi { get; set; }
+        public ICommand DodajDio { get; set; }
+        public ICommand OtkaziDodavanje { get; set; }
+        public ICommand OdbijPonuduDijela { get; set; }
 
-        public ICommand DodavanjeDijela { get; set; }
 
-        public DodavanjeDijelaRadnikViewModel()
+        List<Dio> ponudeniAutodijelovi
         {
-
-            NavigationService = new NavigationService;
-            DodavanjeDijela = new RelayCommand<object>(dodavanjeDijela, true);
+            get
+            {
+                using (var db = new OtpadDbContext())
+                {
+                    return db.ponudeniDijelovi.ToList();
+                }
+            }
         }
 
-        public void dodavanjeDijela(object parametar)
+        public void DodajPonudeniDio()
+        {
+            DodajDio = new RelayCommand<object>(dodaj, mozeLiSeDodati);
+        }
+        public void OdbijPonudeniDio()
+        {
+            OdbijPonuduDijela = new RelayCommand<object>(odbij, mozeLiSeDodati);
+        }
+
+
+        public void dodaj(object parametar)
+        {
+            var db = new OtpadDbContext();
+            db.Dijelovi.Add(); // Dodavanje u listu Dijelovi koja sluzi za Dijelove koji su u prodaji
+        }
+
+        public void odbij(object parametar)
         {
 
+        }
+
+        public bool mozeLiSeDodati(object parametar) // Za sada ovu funkciju koristimo i za odbijanje ponuđenog dijela
+        {
+            return true;
         }
     }
 }
