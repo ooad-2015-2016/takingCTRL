5e99f5c47ef8f8f47bb5c8cde776f65920eaf855
Alma Hodzic <ahodzic3@etf.unsa.ba>
2016-05-09 14:15:33 +0200

    Povezani VM sa V, oni koje sam ja radila (ostale nisam)

--- ProjekatAutootpad/ProjekatAutootpad/Autootpad/Views/ZahtjevZaServisRegistrovaniKupac.xaml.cs
+++ ProjekatAutootpad/ProjekatAutootpad/Autootpad/Views/ZahtjevZaServisRegistrovaniKupac.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProjekatAutootpad.Autootpad.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -25,6 +26,12 @@ namespace ProjekatAutootpad
         public ZahtjevZaServisRegistrovaniKupac()
         {
             this.InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            ZahtjevZaServisRegistrovaniKupacViewModel zahtjevZaServis = e.Parameter as ZahtjevZaServisRegistrovaniKupacViewModel;
+            DataContext = zahtjevZaServis;
         }
     }
 }
